f72ae8c7008346bade65de6891864906566bcfa1
IsaacGarcia <isaac@workker.com.br>
2012-11-14 16:35:18 -0200

    Projeto

---
+++ EHR.Solution/Prototipo/Pagina_principal_do_Sumario_files/axurerp_pagespecificscript.js
@@ -0,0 +1,73 @@
+﻿for(var i = 0; i < 116; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u51'] = 'center';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u55'] = 'center';gv_vAlignTable['u68'] = 'center';gv_vAlignTable['u76'] = 'top';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u32'] = 'top';gv_vAlignTable['u62'] = 'center';gv_vAlignTable['u53'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u104'] = 'center';gv_vAlignTable['u106'] = 'center';document.getElementById('u109_img').tabIndex = 0;
+
+u109.style.cursor = 'pointer';
+$axure.eventManager.click('u109', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('Antimicrobiano.html'), "");
+
+}
+});
+gv_vAlignTable['u60'] = 'top';gv_vAlignTable['u94'] = 'center';gv_vAlignTable['u34'] = 'center';gv_vAlignTable['u64'] = 'top';gv_vAlignTable['u100'] = 'center';gv_vAlignTable['u49'] = 'center';gv_vAlignTable['u112'] = 'center';gv_vAlignTable['u102'] = 'center';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u75'] = 'top';gv_vAlignTable['u66'] = 'top';gv_vAlignTable['u92'] = 'center';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u22'] = 'top';u13.tabIndex = 0;
+
+u13.style.cursor = 'pointer';
+$axure.eventManager.click('u13', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('Pagina_principal_do_Sumario.html'), "");
+
+}
+});
+gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u3'] = 'center';document.getElementById('u99_img').tabIndex = 0;
+
+u99.style.cursor = 'pointer';
+$axure.eventManager.click('u99', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('Outros_Medicamentos_Relevantes.html'), "");
+
+}
+});
+gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u77'] = 'top';gv_vAlignTable['u90'] = 'center';gv_vAlignTable['u20'] = 'center';document.getElementById('u101_img').tabIndex = 0;
+
+u101.style.cursor = 'pointer';
+$axure.eventManager.click('u101', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('Exames.html'), "");
+
+}
+});
+gv_vAlignTable['u98'] = 'center';gv_vAlignTable['u24'] = 'center';document.getElementById('u107_img').tabIndex = 0;
+
+u107.style.cursor = 'pointer';
+$axure.eventManager.click('u107', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('Procedimentos.html'), "");
+
+}
+});
+gv_vAlignTable['u39'] = 'top';gv_vAlignTable['u69'] = 'top';gv_vAlignTable['u114'] = 'center';gv_vAlignTable['u71'] = 'top';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u96'] = 'center';gv_vAlignTable['u35'] = 'top';gv_vAlignTable['u65'] = 'top';document.getElementById('u105_img').tabIndex = 0;
+
+u105.style.cursor = 'pointer';
+$axure.eventManager.click('u105', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('Dados_Gerais.html'), "");
+
+}
+});
+gv_vAlignTable['u110'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u72'] = 'top';gv_vAlignTable['u63'] = 'top';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u70'] = 'top';gv_vAlignTable['u73'] = 'top';gv_vAlignTable['u44'] = 'center';gv_vAlignTable['u78'] = 'top';gv_vAlignTable['u74'] = 'top';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u59'] = 'center';gv_vAlignTable['u108'] = 'center';
