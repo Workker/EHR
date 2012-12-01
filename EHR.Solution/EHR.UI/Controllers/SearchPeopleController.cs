a9ab87851810305d6342337200b75b64e95a75f3
IsaacGarcia <isaac@workker.com.br>
2012-12-01 10:58:52 -0200

    Charts

---
+++ EHR.Solution/EHR.UI/Controllers/SearchPeopleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EHR.UI.Controllers
+{
+    public class SearchPeopleController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
