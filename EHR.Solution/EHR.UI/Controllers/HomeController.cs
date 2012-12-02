78eb5e4bfd7734befdbe32cc11fc6e9d998e5fb4
ThiagoNOliveira <eno.thiago@gmail.com>
2012-12-02 19:12:23 -0200

    Pagina de Login

--- EHR.Solution/EHR.UI/Controllers/HomeController.cs
+++ EHR.Solution/EHR.UI/Controllers/HomeController.cs
@@ -29,5 +29,10 @@ namespace EHR.UI.Controllers
             return
                 "{\"results\":[{\"type\":\"header\",\"text\":\"People\"},{\"type\":\"person\",\"name\":\"Andrew Brookes\", \"imageUrl\":\"../Images/Profiles/1.jpg\"},{\"type\":\"person\",\"name\":\"Leo Coates\", \"imageUrl\":\"../Images/Profiles/2.jpg\"},{\"type\":\"person\",\"name\":\"Reece Davison\", \"imageUrl\":\"../Images/Profiles/3.jpg\"},{\"type\":\"person\",\"name\":\"Lauren Stanley\", \"imageUrl\":\"../Images/Profiles/4.jpg\"},{\"type\":\"person\",\"name\":\"Courtney Herbert\", \"imageUrl\":\"../Images/Profiles/5.jpg\"},{\"type\":\"person\",\"name\":\"Emily Holden\", \"imageUrl\":\"../Images/Profiles/6.jpg\"},{\"type\":\"person\",\"name\":\"Dylan Lynch\", \"imageUrl\":\"../Images/Profiles/7.jpg\"}]}";
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
