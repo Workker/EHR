d1c4e843a6b835ec25c5683cd1ea5bc4680c72de
ThiagoNOliveira <eno.thiago@gmail.com>
2013-01-07 11:10:46 -0200

    Pass of unit test

---
+++ EHR.Solution/EHR.Test/BaseTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EHR.Test.UI
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        [Test]
+        public void test()
+        {
+            Assert.Pass();
+        }
+    }
+}
