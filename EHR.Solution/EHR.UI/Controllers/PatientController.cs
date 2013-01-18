6767116a59ab21f3b2e93c761577296dafc78836
ThiagoNOliveira <eno.thiago@gmail.com>
2013-01-18 15:49:57 -0200

    T660, TA661, TA662, TA663, TA672, TA673, TA674, TA675 - Pronto

--- EHR.Solution/EHR.UI/Controllers/PatientController.cs
+++ EHR.Solution/EHR.UI/Controllers/PatientController.cs
@@ -102,6 +102,8 @@ namespace EHR.UI.Controllers
                 "{\"results\":[{\"type\":\"header\",\"text\":\"People\"},{\"type\":\"person\",\"name\":\"Andrew Brookes\", \"imageUrl\":\"../Images/Profiles/1.jpg\"},{\"type\":\"person\",\"name\":\"Leo Coates\", \"imageUrl\":\"../Images/Profiles/2.jpg\"},{\"type\":\"person\",\"name\":\"Reece Davison\", \"imageUrl\":\"../Images/Profiles/3.jpg\"},{\"type\":\"person\",\"name\":\"Lauren Stanley\", \"imageUrl\":\"../Images/Profiles/4.jpg\"},{\"type\":\"person\",\"name\":\"Courtney Herbert\", \"imageUrl\":\"../Images/Profiles/5.jpg\"},{\"type\":\"person\",\"name\":\"Emily Holden\", \"imageUrl\":\"../Images/Profiles/6.jpg\"},{\"type\":\"person\",\"name\":\"Dylan Lynch\", \"imageUrl\":\"../Images/Profiles/7.jpg\"}]}";
         }
 
+        #region Diagnostic
+
         public PartialViewResult DiagnosticForm()
         {
             return PartialView("GeneralData/_DiagnosticsForm");
@@ -111,11 +113,28 @@ namespace EHR.UI.Controllers
         {
             return PartialView("GeneralData/_DiagnosticTableRow");
         }
-        
-        public void DeleteDiagnostic()
+
+        public void DeleteDiagnostic() { }
+
+        #endregion
+
+        #region Exams
+
+        public PartialViewResult ExamForm()
+        {
+            return PartialView("Exams/_ExamsForm");
+        }
+
+        public PartialViewResult SaveExam(string type, string code, string description)
+        {
+            return PartialView("Exams/_ExamsTableRow");
+        }
+
+        public void DeleteExam()
         {
 
         }
 
+        #endregion
     }
 }
