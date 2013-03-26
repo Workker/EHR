1db872e0f2f27b6b4e41c81c9054718ed2e3528c
SammuelGarcia <sammuel_garcia@hotmail.com>
2013-03-26 11:12:13 -0300

    Implementacao Servico e Lib

---
+++ EHR.Solution/EHR.Domain/Service/GetPatientByHospitalService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EHRIntegracao.Domain.Factorys;
+using EHRIntegracao.Domain.Services;
+using EHRIntegracao.Domain.Services.DTO;
+
+namespace EHR.Domain.Service
+{
+    public class GetPatientByHospitalService
+    {
+        public IList<IPatientDTO> GetPatientBy(DbEnum hospital,IPatientDTO patientDTO)
+        {
+            //var service = new PatientIntegrationService();
+            //return service.GetAllPatients(patientDTO, hospital);
+
+            List<IPatientDTO> pacientes = new List<IPatientDTO>();
+            pacientes.Add(new PatientDTO() { Name = "Sammuel" });
+            pacientes.Add(new PatientDTO() { Name = "Sammuel Garcia" });
+            pacientes.Add(new PatientDTO() { Name = "Thiago" });
+            pacientes.Add(new PatientDTO() { Name = "Thiago Oliveira" });
+            
+            return pacientes.Where(n => n.Name.Contains(patientDTO.Name)).ToList();
+        }
+    }
+}
