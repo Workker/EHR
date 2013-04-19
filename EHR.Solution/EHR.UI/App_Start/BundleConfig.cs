e71d4920dbf52d10cbf2b069ee51a4e757a029e5
ThiagoNOliveira <eno.thiago@gmail.com>
2013-04-19 16:32:29 -0300

    TA764, TA765 - Pronto
    CSS e Html do header

--- EHR.Solution/EHR.UI/App_Start/BundleConfig.cs
+++ EHR.Solution/EHR.UI/App_Start/BundleConfig.cs
@@ -14,11 +14,6 @@ namespace EHR.UI
                 "~/Content/token-input-facebook.css",
                 "~/Content/highslide.css"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
-
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                 "~/Content/themes/base/jquery.ui.core.css",
                 "~/Content/themes/base/jquery.ui.resizable.css",
@@ -33,16 +28,19 @@ namespace EHR.UI
                 "~/Content/themes/base/jquery.ui.progressbar.css",
                 "~/Content/themes/base/jquery.ui.theme.css"));
 
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery.easing.js",
                 "~/Scripts/jquery.collapse.js",
                 "~/Scripts/jquery.tokeninput.js",
                 "~/Scripts/jquery.corner.js",
-                "~/Scripts/AjaxFlagMenu-1.0.2.min.js",
                 "~/Scripts/jquery.tmpl.js",
                 "~/Scripts/knockout-2.1.0.js",
-                "~/Scripts/highslide-with-gallery.js",
                 "~/Scripts/knockout-mapping.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
@@ -51,6 +49,9 @@ namespace EHR.UI
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.unobtrusive*",
                 "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/scripts").Include(
+                "~/Scripts/EHR-Script.js"));
         }
     }
 }
