48e27b958a55624cb9b0d082172c16b29a7a1016
ThiagoNOliveira <eno.thiago@gmail.com>
2013-05-14 16:02:01 -0300

    Teste de procedimento

--- EHR.Solution/EHR.Test/Domain/Entities/ProcedureTest.cs
+++ EHR.Solution/EHR.Test/Domain/Entities/ProcedureTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using EHR.Domain.Entities;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,28 @@ namespace EHR.Test.Domain.Entities
     [TestFixture]
     public class ProcedureTest
     {
+        [Test]
+        public void set_date_witch_sucess()
+        {
+            var procedure = new Procedure();
+            procedure.SetDate(DateTime.Now);
+
+            Assert.IsTrue(procedure.Date != null);
+        }
+
+        [Test]
+        public void get_code_witch_sucess()
+        {
+            var procedure = new Procedure(new Tus() { Code = "03" }, DateTime.Now);
+            Assert.IsTrue(procedure.GetCode() != string.Empty);
+        }
+
+        [Test]
+        public void get_description_witch_sucess()
+        {
+            var procedure = new Procedure(new Tus() { Description = "desc" }, DateTime.Now);
+            Assert.IsTrue(procedure.GetDescription() != string.Empty);
+        }
 
     }
 }
