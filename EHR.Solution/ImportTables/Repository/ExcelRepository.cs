56521ba6aaf5fc4ec5392de6ef26b30de3430a09
Thiago N. Oliveira <eno.thiago@gmail.com>
2013-09-18 14:19:34 -0300

    US367, US364 - Obtenção dos dados da planilha do excel.

---
+++ EHR.Solution/ImportTables/Repository/ExcelRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+namespace ImportTables.Repository
+{
+    public class ExcelRepository
+    {
+        public DataTable GetAllDataFrom(string path, string fileName, string sheetName)
+        {
+            var file = string.Format("{0}\\" + fileName, path);
+            var connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", file);
+
+            var adapter = new OleDbDataAdapter("SELECT * FROM [" + sheetName + "$]", connectionString);
+            var ds = new DataSet();
+
+            adapter.Fill(ds, "Table");
+
+            return ds.Tables["Table"];
+        }
+    }
+}
