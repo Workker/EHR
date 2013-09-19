fdb46d63e3514105a3438f30180af481b2fdfcab
Thiago N. Oliveira <eno.thiago@gmail.com>
2013-09-19 08:16:19 -0300

    AllergyTypeEnum

---
+++ EHR.Solution/EHR.Domain/Entities/AllergyTypeEnum.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace EHR.Domain.Entities
+{
+    public enum AllergyTypeEnum : short
+    {
+        [Description("Angioedema")]
+        Angioedema = 1,
+        [Description("Urticária")]
+        Urticaria = 2,
+        [Description("Choque Anafilático")]
+        ChoqueAnafilatico = 3,
+        [Description("Broncoespasmo")]
+        Broncoespasmo = 4,
+        [Description("Laringoespasmo")]
+        Laringoespasmo = 5,
+        [Description("Outros")]
+        Outros = 6
+    }
+}
