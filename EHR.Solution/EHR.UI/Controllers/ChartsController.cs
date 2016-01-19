319badef31c3bb4c1076a310482da698a247ced7
Isaac Garcia <isaac@workker.com.br>
2016-01-18 23:42:31 -0200

    Grafico de prescrições

---
+++ EHR.Solution/EHR.UI/Controllers/ChartsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EHR.UI.Controllers
+{
+    public class pizza
+    {
+
+    }
+
+    public class ChartsController : System.Web.Mvc.Controller
+    {
+        //
+        // GET: /Charts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
