91fefb0b7fb297bf29b1186e176e45f54513e856
Isaac Garcia <isaac@workker.com.br>
2016-01-19 22:39:58 -0200

    EHR Analytics

---
+++ EHR.Solution/EHR.Analytics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EHR.Analytics.Startup))]
+namespace EHR.Analytics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
